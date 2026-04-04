dfdb996ad654fac3c760438c8e93f18c1df9d87b
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-04-04 06:44:21 +0100

    Create init.sh

---
+++ bin/init.sh
@@ -0,0 +1,10 @@
+brew install git-xet
+git xet install
+git clone https://huggingface.co/spaces/
+QUBUHUB/Lmlm
+docker run -it -p 7860:7860 --platform=linux/amd64 \
+	registry.hf.space/qubuhub-lmlm:cpu-758d37f python app.py
+git clone https://huggingface.co/spaces/QUBUHUB/Lmlm
+cd Lmlm
+pip install -r requirements.txt
+python app.py
